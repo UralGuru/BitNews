fcbe10f4d14ca43728dc3b4ab484ea10f34db099
UralGuru <gizzatullinu@gmail.com>
2023-01-07 00:29:21 +0500

    CreateConstants

---
+++ src/constants/types.ts
@@ -0,0 +1,17 @@
+export interface NewPost {
+    title: string,
+    content: string,
+    id: number,
+    createdAt: string,
+    updatedAt: string
+}
+
+export interface Theme {
+    id: number,
+    name: string,
+    mainColor: string,
+    secondColor: string,
+    title: string,
+    textColor: string
+}
+
