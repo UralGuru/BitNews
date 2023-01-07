91ab99a3bffa3c8c27a69b433442f8bad315b319
UralGuru <gizzatullinu@gmail.com>
2023-01-07 14:53:50 +0500

    CreateHome&ThemePages

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './home.module.css'
+
+const Home = () => {
+    return (
+        <div>
+            HomePage
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/theme/theme.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './theme.module.css'
+
+const Theme = () => {
+    return (
+        <div>
+            HomePage
+        </div>
+    );
+};
+
+export default Theme;
