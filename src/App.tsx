7b2c5a2c73c9f464c378885466cb1c824d7bc962
Ural <gizzatullinu@gmail.com>
2023-01-10 14:05:18 +0500

    RefactorHomePage

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/navbar/navbar';
 import { URLManager } from './shared/url-manager';
 import Theme from './pages/theme/theme';
-import Home from './pages/home/home';
+import Home from "./pages/home/home";
 import store from './store/store';
-import HomeScroll from "./pages/homeScroll/homeScroll";
 
 
 function App() {
     return (
         <Provider store={store}>
             <Routes>
-                <Route path={URLManager.getHomeURL()} element={<HomeScroll/>}/>
+                <Route path={URLManager.getHomeURL()} element={<Home/>}/>
                 <Route path={URLManager.getThemeURL()} element={<Theme/>}/>
             </Routes>
             <Navbar/>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { getNewsThunk, incrementPage } from '../../store/slices/newsSlices';
-import { useAppDispatch, useAppSelector } from '../../shared/hooks';
-import NewsCard from '../../components/newsCard/newsCard';
-import { NewsItem } from '../../constants/types';
-import styles from './home.module.css';
+import React, {useEffect} from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
+import {PullDownContent, PullToRefresh, RefreshContent, ReleaseContent} from 'react-js-pull-to-refresh';
+import {getNewsThunk, incrementPage, updateNews} from "../../store/slices/newsSlices";
+import {useAppDispatch, useAppSelector} from "../../shared/hooks";
+import NewsCard from "../../components/newsCard/newsCard";
+import {NewsItem} from "../../constants/types";
+import styles from './homeScroll.module.css';
 
 
 const Home = () => {
@@ -12,6 +13,14 @@ const Home = () => {
     const theme = useAppSelector((state)=> state.theme)
     const newsState = useAppSelector((state) => state.news)
     const dataNewsArray: NewsItem[] = Object.assign([], Object.values(newsState.newsItems))
+
+    const onRefresh = () =>{
+        return new Promise((resolve) => {
+            setTimeout(resolve, 200);
+            dispatch(updateNews())
+            dispatch(getNewsThunk())
+        })
+    }
 
     useEffect(() => {
         dispatch(getNewsThunk())
@@ -23,22 +32,36 @@ const Home = () => {
 
     return (
         <div className={styles.homePage}>
-            <InfiniteScroll
-                dataLength={dataNewsArray.length}
-                next={() => {
-                    dispatch(incrementPage())
-                    dispatch(getNewsThunk())
-                }}
-                hasMore={newsState.hasMore}
-                loader={<h4>Loading...</h4>}
+            <PullToRefresh
+                pullDownContent={<PullDownContent />}
+                releaseContent={<ReleaseContent />}
+                refreshContent={<RefreshContent />}
+                pullDownThreshold={200}
+                onRefresh={onRefresh}
+                triggerHeight={40}
+                backgroundColor={theme.mainColor}
             >
-                {dataNewsArray.map(news => <NewsCard key={crypto.randomUUID()}
-                                                     id={news.id}
-                                                     title={news.title}
-                                                     content={news.content}
-                                                     createdAt={news.createdAt}
-                                                     updatedAt={news.updatedAt}/>)}
-            </InfiniteScroll>
+                <div className={styles.basicContainer} id="basic-container">
+                    <div className={styles.basicLabel} id="basic-label" onClick={onRefresh}>потяни🙃нажми </div>
+                </div>
+                <InfiniteScroll
+                    dataLength={dataNewsArray.length}
+                    next={() => {
+                        dispatch(incrementPage())
+                        dispatch(getNewsThunk())
+                    }}
+                    hasMore={newsState.hasMore}
+                    loader={<h4>Loading...</h4>}
+                >
+                    {dataNewsArray.map(news => <NewsCard key={crypto.randomUUID()}
+                                                         id={news.id}
+                                                         title={news.title}
+                                                         content={news.content}
+                                                         createdAt={news.createdAt}
+                                                         updatedAt={news.updatedAt}/>)}
+                </InfiniteScroll>
+            </PullToRefresh>
+
         </div>
     );
 };
